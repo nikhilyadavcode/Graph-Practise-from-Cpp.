ecc6e757313ff96a55bacda1f66bf833c0b023f4
nikhilyadavcode <147080866+nikhilyadavcode@users.noreply.github.com>
2025-09-09 19:19:34 +0530

    Topologist sort using BFS

---
+++ TopologicalSortBFS.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>topologistSort(int V,vector<vector<int>>&adj){
+
+    vector<int>indegree(V,0);
+
+    for(int i=0;i<V;i++){
+        for(int v:adj[i]){
+            indegree[v]++;
+        }
+    }
+    queue<int>q;
+    for(int i=0;i<V;i++){
+        if(indegree[i]==0)
+        q.push(i);
+    }
+    vector<int>topo;
+    while(!q.empty()){
+        int u=q.front();
+        q.pop();
+        topo.push_back(u);
+
+        for(int V:adj[u]){
+            indegree[V]-- ;
+            if(indegree[V]==0){
+                q.push(V);
+            }
+        }
+    }
+    if((int)topo.size()!=V){
+        cout<<"cycle detected! No valid opologiscal order";
+        return {};
+    }
+    return topo;
+}
+int main(){
+    int V=6;
+
+    vector<vector<int>>adj(V);
+
+    adj[5].push_back(2);
+    adj[5].push_back(0);
+    adj[4].push_back(0);
+    adj[4].push_back(1);
+    adj[2].push_back(3);
+    adj[3].push_back(1);
+
+    vector<int>topo=topologistSort(V,adj);
+    cout<<"Topologist Sort Using BFS:";
+
+    for(int node:topo){
+        cout<<node<<" ";
+    }
+    cout<<endl;
+}
